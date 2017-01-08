bfd9dc83bc8622c796c8f9950069a40530f28deb
motoaleks <kerzkon@gmail.com>
2017-01-08 12:58:15 +0300

    main form v.1.0.1. Added preview, remade scene creation

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -3,11 +3,10 @@ package View;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 
 
 public class Controller {
@@ -19,16 +18,13 @@ public class Controller {
     private URL location;
 
     @FXML
-    private Label lb_progressName;
-
-    @FXML
-    private HBox lv_files;
-
-    @FXML
-    private ProgressBar pb_progressStatus;
+    private ListView<?> lv_files;
 
     @FXML
     private RadioButton rb_fileSearch;
+
+    @FXML
+    private TextArea ta_preview;
 
     @FXML
     private TextField txt_search;
@@ -36,11 +32,10 @@ public class Controller {
 
     @FXML
     void initialize() {
-        assert lb_progressName != null : "fx:id=\"lb_progressName\" was not injected: check your FXML file 'View.fxml'.";
-        assert lv_files != null : "fx:id=\"lv_files\" was not injected: check your FXML file 'View.fxml'.";
-        assert pb_progressStatus != null : "fx:id=\"pb_progressStatus\" was not injected: check your FXML file 'View.fxml'.";
-        assert rb_fileSearch != null : "fx:id=\"rb_fileSearch\" was not injected: check your FXML file 'View.fxml'.";
-        assert txt_search != null : "fx:id=\"txt_search\" was not injected: check your FXML file 'View.fxml'.";
+        assert lv_files != null : "fx:id=\"lv_files\" was not injected: check your FXML file 'main.fxml'.";
+        assert rb_fileSearch != null : "fx:id=\"rb_fileSearch\" was not injected: check your FXML file 'main.fxml'.";
+        assert ta_preview != null : "fx:id=\"ta_preview\" was not injected: check your FXML file 'main.fxml'.";
+        assert txt_search != null : "fx:id=\"txt_search\" was not injected: check your FXML file 'main.fxml'.";
 
 
     }
--- src/View/Main.java
+++ src/View/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(root);
         primaryStage.setTitle("FileSearch");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
