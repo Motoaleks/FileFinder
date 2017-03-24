dc78e54382d7f3a8579e183c01411ad0b0b1ed41
motoaleks <kerzkon@gmail.com>
2017-03-25 01:31:40 +0300

    Nebola v.2.2
    Added index storage dynamical loading after name and parameters already loaded
    .
    - added dynamical storage loading
    - added STF library for future use# Please enter the commit message for your changes. Lines starting

--- src/finder/java/index/Index.java
+++ src/finder/java/index/Index.java
@@ -15,8 +15,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.RandomAccessFile;
 import java.io.Serializable;
 import java.util.logging.Logger;
+import org.nustaq.serialization.FSTConfiguration;
 
 /**
  * Created by: Aleksandr
@@ -27,12 +29,19 @@ import java.util.logging.Logger;
  */
 public class Index implements Serializable {
 
+  private static final FSTConfiguration conf = FSTConfiguration.createDefaultConfiguration();
   private transient static Logger log;
+
+  static {
+    log = Logger.getLogger(Index.class.getName());
+  }
+
   private transient IndexingHandler handler;
   private IndexStorage storage;
   private IndexParameters parameters;
   private String name;
 
+  // ===============  Constructors
   private Index() {
 
   }
@@ -46,40 +55,78 @@ public class Index implements Serializable {
     this.parameters = parameters;
     handler = new IndexingHandler(this);
     storage = new InvertedIndex(parameters);
-    log = Logger.getLogger(Index.class.getName());
     // todo: redo name generating
     name = "test";
   }
 
-  public static Index load(String name) {
-    String filename = name;
+  // ===============  Saving
 
+  public static Index load(String name) {
     Index index = null;
     try {
-      FileInputStream fileIn = null;
-      ObjectInputStream in = null;
-      fileIn = new FileInputStream(filename);
-      in = new ObjectInputStream(fileIn);
-      index = (Index) in.readObject();
+      // creating streams
+      FileInputStream fileIn = new FileInputStream(new RandomAccessFile(name, "rw").getFD());
+      ObjectInputStream in = new ObjectInputStream(fileIn);
 
-      index.handler = new IndexingHandler(index);
-      index.storage.parameters = index.parameters;
-      if (Index.log == null) {
-        log = Logger.getLogger(Index.class.getName());
-      }
+      // red name + parameters (it's fast operation)
+      String indexName = (String) in.readObject();
+      IndexParameters indexParameters = (IndexParameters) in.readObject();
+      // creating index with specified name + parameters
+      index = new Index(indexName, indexParameters);
 
-      fileIn.close();
-      in.close();
-
-      log.info("Index " + name + " loaded.");
-    } catch (IOException e) {
-      e.printStackTrace();
-      log.severe("File-Index " + filename + " was not loaded, problem acquired:" + e.getMessage());
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
+      // make final index
+      Index finalIndex = index;
+      // run thread with long operation - loading index storage
+      new Thread(() -> {
+        try {
+          // load storage
+          finalIndex.storage = (IndexStorage) in.readObject();
+          // say info
+          log.info("Index " + name + " loaded.");
+        } catch (IOException | ClassNotFoundException e) {
+          log.severe("File-Index " + name + " was not loaded, problem acquired:" + e.getMessage());
+        } finally {
+          try {
+            // closing streams
+            fileIn.close();
+            in.close();
+          } catch (IOException e) {
+            log.fine(
+                "File-Index " + name + " was loaded, but streams were not closed. Problem acquired:" + e.getMessage());
+          }
+        }
+      }).start();
+    } catch (Exception e) {
+      log.severe("File-Index " + name + " was not loaded, problem acquired:" + e.getMessage());
     }
     return index;
   }
+
+  public void save(String directory) {
+    try {
+      // create filename
+      String filename = directory + name + ".ser";
+      // open streams
+      FileOutputStream fileOut = new FileOutputStream(new RandomAccessFile(filename, "rw").getFD());
+      ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+      // write objects in order - name, parameters, storage
+      out.writeObject(name);
+      out.writeObject(parameters);
+      out.writeObject(storage);
+
+      // close streams
+      out.flush();
+      out.close();
+      fileOut.flush();
+      fileOut.close();
+      log.info("Index " + name + " saved to " + filename);
+    } catch (IOException i) {
+      log.severe("Index " + name + " was not saved, problem acquired:" + i.getMessage());
+    }
+  }
+
+  // ===============  Operations
 
   public void index(IndexingRequest request) {
     log.info("Indexing with request \"" + request.getId().toString() + "\" started");
@@ -91,26 +138,13 @@ public class Index implements Serializable {
     storage.search(request);
   }
 
+  // ===============  Getters/Setters
   public IndexStorage getStorage() {
     return storage;
   }
 
   public IndexParameters getParameters() {
     return parameters;
-  }
-
-  public void save(String directory) {
-    try {
-      String filename = directory + name + ".ser";
-      FileOutputStream fileOut = new FileOutputStream(filename);
-      ObjectOutputStream out = new ObjectOutputStream(fileOut);
-      out.writeObject(this);
-      out.close();
-      fileOut.close();
-      log.info("Index " + name + " saved to " + filename);
-    } catch (IOException i) {
-      log.severe("Index " + name + " was not saved, problem acquired:" + i.getMessage());
-    }
   }
 
   public String getName() {
